0bdfc5d1580b5b60ade35d964ab543f9dad17abc
OskarFalk <Oskarffc@gmail.com>
2022-02-22 08:31:18 +0100

    GameLogic Improved

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,23 +1,12 @@
 var Chocolates = 13;
 var playerLastLick;
 
-function Lick1Chocolate(){
-    Chocolates--;
-    playerLastLick = 1;
-    console.log("1 Chocolate was taken");
+function LickChocolate(Count){
+    Chocolates -= Count;
+    playerLastLick = Count;
+    console.log(Count + " Chocolate was taken");
 }
 
-function Lick2Chocolate(){
-    Chocolates -= 2;
-    playerLastLick = 2;
-    console.log("2 Chocolate was taken");
-}
-
-function Lick3Chocolate(){
-    Chocolates -= 3;
-    playerLastLick = 3;
-    console.log("3 Chocolate was taken");
-}
 
 function PlayerTurn(){
     NormalizeButtons();
